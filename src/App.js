a11d53cc85c42981c9643de5ef5243c00e6111c8
Chris Ling <kurisuringu@gmail.com>
2021-04-17 19:32:49 -0700

    Add roadmap to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+/**
+ * Roadmap
+ * =======
+ * 
+ *  Services (none)
+ *  Infra (none)
+ * 
+ *  Objects
+ *   Die - used to display value of roll
+ *   Log - used to store history of past rolls
+ *   Selector - used to select sides for each die
+ *   Button - used to add die, remove die, roll dice, selects sides for die
+ * 
+ *  Functions
+ *   addDie - Adds a new die to state with the requisite sides
+ *   removeDie - Removes the specific die from the existing dice
+ *   rollDie - Rolls all the existing dice together
+ *    calculateTotal - Calculate total for all dice; dependent on rollDie
+ *    updateLog - Update log with state information + total; dependent on rollDie
+ */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world!
     </div>
   );
 }
